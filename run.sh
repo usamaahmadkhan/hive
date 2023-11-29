c62c853a7d90d8b12594132dc69749870fbd3a3c
Usama Ahmad <usama.ahmad.khan@hotmail.com>
2023-11-29 14:11:39 +0500

    add run.sh script

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 # make GO_REQUIRED_MIN_VERSION:= build
 
 # scale down hive operator
-oc scale deploy hive-operator -n open-cluster-management --replicas=0
+oc scale deploy hive-operator -n multicluster-engine --replicas=0
 
 # scale down hive controllers
 oc scale -n hive deployment.v1.apps/hive-controllers --replicas=0
@@ -13,9 +13,10 @@ oc scale -n hive deployment.v1.apps/hive-controllers --replicas=0
 # LOG_LEVEL="debug" HIVE_CLUSTERSYNC_POD_NAME="hive-clustersync-0 "HIVE_NS="hive"
 # HIVE_CLUSTERSYNC_POD_NAME="hive-clustersync" HIVE_NS="hive" make GO_REQUIRED_MIN_VERSION:= run
 
-HIVE_CLUSTERSYNC_POD_NAME="hive-clustersync-0" HIVE_NS="hive" ./bin/manager --controllers hibernation,clusterDeployment --log-level=debug
+HIVE_CLUSTERSYNC_POD_NAME="hive-clustersync-0" HIVE_NS="hive" ./bin/manager --controllers hibernation,clusterDeployment --log-level=debug | grep cluster-stakater-nonprod  | tee output.log
 
 
 
 
-# oc -n stakater-devtest patch cd stakater-devtest --type='merge' -p $'spec:\n powerState: Hibernating'
+
+# oc -n cluster-stakater-nonprod patch cd stakater-nonprod --type='merge' -p $'spec:\n powerState: Hibernating'
